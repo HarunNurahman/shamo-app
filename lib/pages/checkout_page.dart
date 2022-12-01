9e127e4a7c16e84b5d349fe519e5b11da455ad5b
Harun Nurahman <harun.nurrahman@hotmail.com>
2022-12-01 15:48:34 +0700

    Styling Checkout Page

--- lib/pages/checkout_page.dart
+++ lib/pages/checkout_page.dart
@@ -34,32 +34,35 @@ class CheckoutPage extends StatelessWidget {
 
     Widget checkoutContent() {
       return ListView(
-        padding: EdgeInsets.all(defaultMargin),
+        padding: EdgeInsets.symmetric(horizontal: defaultMargin),
         children: [
           // List Items
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'List Item(s)',
-                style: primaryTextStyle.copyWith(
-                  fontSize: 16,
-                  fontWeight: medium,
+          Padding(
+            padding: EdgeInsets.only(top: defaultMargin),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'List Item(s)',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
                 ),
-              ),
-              const CheckoutCard(
-                imgUrl: 'assets/images/img_shoes-7.png',
-                productName: 'Terrex Urban Low',
-                productPrice: 143.98,
-                quantity: 2,
-              ),
-              const CheckoutCard(
-                imgUrl: 'assets/images/img_shoes-4.png',
-                productName: 'Court Vision 2.0 Shoes Limited Edition',
-                productPrice: 57.15,
-                quantity: 1,
-              ),
-            ],
+                const CheckoutCard(
+                  imgUrl: 'assets/images/img_shoes-7.png',
+                  productName: 'Terrex Urban Low',
+                  productPrice: 143.98,
+                  quantity: 2,
+                ),
+                const CheckoutCard(
+                  imgUrl: 'assets/images/img_shoes-4.png',
+                  productName: 'Court Vision 2.0 Shoes Limited Edition',
+                  productPrice: 57.15,
+                  quantity: 1,
+                ),
+              ],
+            ),
           ),
 
           // Shipment Information
@@ -137,6 +140,113 @@ class CheckoutPage extends StatelessWidget {
                         ],
                       ),
                     ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+          // Payment Summary
+          Container(
+            margin: EdgeInsets.only(top: defaultMargin),
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: bgColor4,
+              borderRadius: BorderRadius.circular(defaultRadius),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Payment Summary',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+                SizedBox(height: defaultRadius),
+                // Product Quantity
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Product Quantity',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text(
+                      '2 Item(s)',
+                      style: primaryTextStyle.copyWith(
+                        fontWeight: medium,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: defaultRadius),
+
+                // Product Price
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Product Price',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text(
+                      '\$575.96',
+                      style: primaryTextStyle.copyWith(
+                        fontWeight: medium,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: defaultRadius),
+
+                // Shipping Price
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Shipping Price',
+                      style: secondaryTextStyle.copyWith(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text(
+                      'Free',
+                      style: primaryTextStyle.copyWith(
+                        fontWeight: medium,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: defaultRadius),
+
+                // Divider
+                Divider(thickness: 1, color: priceColor),
+                SizedBox(height: defaultRadius),
+
+                // Total Shopping
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Total',
+                      style: priceTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: semibold,
+                      ),
+                    ),
+                    Text(
+                      '\$575.92',
+                      style: priceTextStyle.copyWith(
+                        fontSize: 14,
+                        fontWeight: semibold,
+                      ),
+                    )
                   ],
                 ),
               ],
